18af102b02eddd7cb450693450e353de680bbf8a
smoothlyraining <tomas.sarmiento@gmail.com>
2020-03-02 14:07:45 -0300

    listo

---
+++ Pitagoras.java
@@ -0,0 +1,5 @@
+public class Pitagoras{
+    public double calcularHipotenusa(int legA, int legB){
+        return(Math.hypot(legA, legB));
+    }
+}
---
+++ PitagorasTest.java
@@ -0,0 +1,7 @@
+public class PitagorasTest{
+    public static void main(String[] args){
+        Pitagoras iD = new Pitagoras();
+        double hipotenusa = iD.calcularHipotenusa(5,12);
+        System.out.println(hipotenusa);
+    }
+}
